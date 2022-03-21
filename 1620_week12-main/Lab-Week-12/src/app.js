17c53c6b984e8b2d551a4184c4dde8a7cfb1fd89
FZHU <fzhu9@my.bcit.ca>
2022-03-21 11:47:55 -0700

    added dark mode to app.js

--- 1620_week12-main/Lab-Week-12/src/app.js
+++ 1620_week12-main/Lab-Week-12/src/app.js
@@ -1,5 +1,17 @@
+//Part 2
 function myFunction(){
     const selectedelement = document.querySelector(".one")
     return selectedelement
 }
 console.log(myFunction())
+
+//Part 3
+
+
+//Part 4
+function makeDarkMode(){
+    const DM = document.querySelector('div')
+    DM.classList.add('dark-theme')
+}
+console.log(makeDarkMode())
+
