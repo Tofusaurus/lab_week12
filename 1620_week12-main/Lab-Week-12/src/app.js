f3ba5b4c0e281f4af8c60f543165bb099f3529c6
FZHU <fzhu9@my.bcit.ca>
2022-03-24 14:26:23 -0700

    adding function to part 3. grade average/scholarship

--- 1620_week12-main/Lab-Week-12/src/app.js
+++ 1620_week12-main/Lab-Week-12/src/app.js
@@ -7,7 +7,28 @@ console.log(myFunction())
 
 //Part 3
 
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+ 
+function scholarship(new_list) {
 
+    const studentsScholarship = []
+    
+    for (let value of new_list) {
+        value.grades = value.grades.reduce((a,b) => a + b, 0) / value.grades.length; {
+            if (value.grades >= 80) {
+                value.gradeAvg = value.grades;
+                studentsScholarship.push(value)
+            }
+        }
+    }
+    return studentsScholarship
+}
+console.log(scholarship(students))
 
 //Part 4
 function makeIncognito(){
